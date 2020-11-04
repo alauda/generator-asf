6bc31673ffe182103df67a3b95cce1bf3c9ea087
v_vjyhan <v_vjyhan@tencent.com>
2020-11-04 19:01:33 +0800

    fix: streamline create command

--- cli/asf.js
+++ cli/asf.js
@@ -1,14 +1,20 @@
 #!/usr/bin/env node
 
-const path = require('path');
-const Environment = require('yeoman-environment');
+const path = require("path");
+const Environment = require("yeoman-environment");
 
 let env = Environment.createEnv();
-env.lookup({ packagePaths: [path.join(__dirname, '..')] });
-const [,,action, ...args] = process.argv
+env.lookup({ packagePaths: [path.join(__dirname, "..")] });
+const [, , action, ...args] = process.argv;
 
-args.push['--no-insight']
-const argv = require('minimist')(args)
+args.push("--no-insight");
+const argv = require("minimist")(args);
+const actions = {
+    cs: "create-service",
+    cg: "create-gateway"
+};
 
-env.run([`asf:${action}`,...args],{help:argv.h || argv.help,...argv})
-
+env.run([`asf:${actions[action] || action}`, ...args], {
+    help: argv.h || argv.help,
+    ...argv
+});
