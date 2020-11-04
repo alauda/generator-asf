fd1fb45bd0fe9b955aee7d0cf7693f429335fc13
gaodawei <dwgao@alauda.io>
2020-11-04 11:57:20 +0800

    fix tree command not found

--- generators/create-gateway/index.js
+++ generators/create-gateway/index.js
@@ -8,7 +8,7 @@ const camelcase = require('camelcase');
 const decamelize = require('decamelize');
 const walk = require('walk');
 const path = require('path');
-const spawn = require('child_process').spawn;
+const tree = require('tree-node-cli');
 const fs=require('fs')
 const alaudaSay = require('../../lib/alauda-say');
 
@@ -113,35 +113,35 @@ module.exports = class extends Generator {
     }
 
     end() {
-        if(!this.props.bootstrap) process.exit(0);
+        if(!this.props.bootstrap) return;
         if(this.options["config"]){
             fs.writeFileSync(this.options["config"],JSON.stringify(this.props, null, 4));
         }
         this.log("\n");
         this.log(chalk.green("您已成功完成工程创建。工程结构如下："));
-        let tree = spawn('tree');
-        tree.stdout.on('data', data => 
-            this.log(chalk.yellow(data.toString()))
-        );
-        tree.stdout.on('close', data => {
-                this.log(chalk.green("脚手架已为您完成所有依赖的添加，您可能还需要根据实际开发环境完成配置文件修改。"))
-                if(this.props.configServerEnabled){
-                    let configPath = chalk.hex("#34AFE4")(`【${this.props.projectName}/src/main/resources/config/application.yml】`)
-                    let kv = chalk.hex("#34AFE4")(`【config/${this.props.projectName}/data】`);
-                    if(this.props.configServerType == 'K8S'){
-                        configPath = chalk.hex("#34AFE4")(`【${this.props.projectName}/src/main/resources/config/configmap.yml】`)
-                        let namespace = chalk.hex("#34AFE4")(`【${this.props.k8sNamespace}】`);
-                        let configmap = chalk.hex("#34AFE4")(`【${this.props.projectName}】`);
-                        this.log(`${chalk.yellow('请将配置文件')}${configPath}${chalk.yellow('提交至 Kubernetes 集群的 ')}${namespace}${chalk.yellow('命名空间下')}${configmap}${chalk.yellow('ConfigMap 中')}`)
-                    }else{
-                        this.log(`${chalk.yellow('请将配置文件')}${configPath}${chalk.yellow('提交至 Consul 的 ')}${kv}${chalk.yellow('Key/Value 配置中')}`)
-                    }
-                }
-                
-                this.log(chalk.green("祝您开发顺利！如对脚手架有任何建议，您可以通过提 github issues(https://github.com/madogao/generator-asf) 或发送邮件到 suggest@alauda.io 进行反馈，我们会及时回复并处理。"))
-                chalk.reset();
-                process.exit(0);
+        let treeString = tree(this.props.projectName,
+            {
+                allFiles: true,
+                exclude: [/node_modules/, /lcov/],
+                maxDepth: 4,
             }
         );
+        this.log(chalk.yellow(treeString))
+        this.log(chalk.green("脚手架已为您完成所有依赖的添加，您可能还需要根据实际开发环境完成配置文件修改。"))
+        if(this.props.configServerEnabled){
+            let configPath = chalk.hex("#34AFE4")(`【${this.props.projectName}/src/main/resources/config/application.yml】`)
+            let kv = chalk.hex("#34AFE4")(`【config/${this.props.projectName}/data】`);
+            if(this.props.configServerType == 'K8S'){
+                configPath = chalk.hex("#34AFE4")(`【${this.props.projectName}/src/main/resources/config/configmap.yml】`)
+                let namespace = chalk.hex("#34AFE4")(`【${this.props.k8sNamespace}】`);
+                let configmap = chalk.hex("#34AFE4")(`【${this.props.projectName}】`);
+                this.log(`${chalk.yellow('请将配置文件')}${configPath}${chalk.yellow('提交至 Kubernetes 集群的 ')}${namespace}${chalk.yellow('命名空间下')}${configmap}${chalk.yellow('ConfigMap 中')}`)
+            }else{
+                this.log(`${chalk.yellow('请将配置文件')}${configPath}${chalk.yellow('提交至 Consul 的 ')}${kv}${chalk.yellow('Key/Value 配置中')}`)
+            }
+        }
+        
+        this.log(chalk.green("祝您开发顺利！如对脚手架有任何建议，您可以通过提 github issues(https://github.com/madogao/generator-asf) 或发送邮件到 suggest@alauda.io 进行反馈，我们会及时回复并处理。"))
+        chalk.reset();
     }
 };
